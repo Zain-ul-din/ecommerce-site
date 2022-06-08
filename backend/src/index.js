53746aaff0163a6828e63ef71e9880952230cc60
Zain Ul Din <zain.personal47@gmail.com>
2022-06-08 22:36:17 +0500

    backend

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express()
+
+// Listen to server
+
+console.log('Welcome to ecommerce apis')
+
+app.listen(8000 , ()=>{
+    console.log(`server is running on port 8000`)
+})
+
+
+
